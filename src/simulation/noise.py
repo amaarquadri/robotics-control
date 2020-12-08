0a1b79e5abb45a8b276871bf8697c9ed584c47cf
Amaar Quadri <amaarquadri@gmail.com>
2020-12-08 17:24:52 -0500

    added smooth and random noise options

--- src/simulation/noise.py
+++ src/simulation/noise.py
@@ -1,9 +1,10 @@
 from opensimplex import OpenSimplex
 from scipy import stats
+import numpy as np
 from time import time
 
 
-def noise_function(mean=0, std=1, velocity=1, seed=None):
+def smooth_gaussian_noise_function(means=np.array([0]), variances=np.array([[1]]), velocity=1, seed=3):
     """
     Creates a function that generates a smoothly varying random variable with the given mean and standard deviation.
     The velocity is the rate at which the resulting function will move through the OpenSimplex space.
@@ -11,15 +12,29 @@ def noise_function(mean=0, std=1, velocity=1, seed=None):
     if seed is None:
         seed = int(time() * 1e6)
 
-    noise_generator = OpenSimplex(seed)
+    noise_generators = np.array([OpenSimplex(seed * (i + 1)) for i in range(len(means))])
 
     def noise(t):
         """
         Calculates a noise value for the given time.
         """
-        noise_value = noise_generator.noise2d(t * velocity, 0)
-        cdf_value = (noise_value + 1) / 2  # map (-1, 1) to (0, 1)
-        gaussian_value = stats.norm.ppf(cdf_value, loc=mean, scale=std)
-        return gaussian_value
+        noise_values = np.array([noise_generator.noise2d(t * velocity, 0) for noise_generator in noise_generators])
+        cdf_values = (noise_values + 1) / 2  # map (-1, 1) to (0, 1)
+        # TODO: properly use covariance matrix instead of just diagonal
+        gaussian_values = np.array([stats.norm.ppf(cdf_value, loc=mean, scale=np.sqrt(variance))
+                                    for cdf_value, mean, variance in zip(cdf_values, means, np.diagonal(variances))])
+        return gaussian_values
+
+    return noise
+
+
+def gaussian_noise_function(means=np.array([0]), variances=np.array([[1]])):
+    """
+    Creates a function that generates a random variable with the given mean and standard deviation.
+    """
+    def noise(_):
+        # TODO: properly use covariance matrix instead of just diagonal
+        return np.array([np.random.normal(mean, np.sqrt(variance))
+                         for mean, variance in zip(means, np.diagonal(variances))])
 
     return noise
--- src/simulation/simulator.py
+++ src/simulation/simulator.py
@@ -1,12 +1,15 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+from src.simulation.noise import smooth_gaussian_noise_function, gaussian_noise_function
 
 
-def simulate(physics_system, C, controller, state_0=None, t_f=10, rtol=1e-3):
+def simulate(physics_system, C, controller, V=None, W=None, state_0=None, t_f=10, rtol=1e-3):
     """
     :param physics_system:
     :param C:
     :param controller: The controller function which accepts (t, x_hat, y) and returns (u, x_hat_dot).
+    :param V:
+    :param W:
     :param state_0: A numpy array of the starting x and x_hat values.
     :param t_f:
     :param rtol:
@@ -14,16 +17,21 @@ def simulate(physics_system, C, controller, state_0=None, t_f=10, rtol=1e-3):
     if state_0 is None:
         state_0 = np.zeros(2 * physics_system.x_dim)
 
-    def state_derivative(t, state):
-        x = state[:physics_system.x_dim]
-        x_hat = state[physics_system.x_dim:]
+    model_noise = gaussian_noise_function(variances=V)
+    control_noise = gaussian_noise_function(variances=W)
 
-        y = np.dot(C, x)
+    def state_derivative(t, state):
+        print(t)
+        midpoint = len(state) // 2
+        x = state[:midpoint]
+        x_hat = state[midpoint:]
+
+        y = np.dot(C, x) + control_noise(t)
         u, x_hat_dot = controller(t, x_hat, y)
-        x_dot = physics_system.equations_of_motion_func(x, u)
+        x_dot = physics_system.equations_of_motion_func(x, u) + model_noise(t)
 
         return np.concatenate((x_dot, x_hat_dot))
 
-    result = solve_ivp(state_derivative, (0, t_f), state_0, method='RK45', rtol=rtol)
+    result = solve_ivp(state_derivative, (0, t_f), state_0, method='RK45', rtol=1e99, atol=1e99, max_step=0.001)
     # noinspection PyUnresolvedReferences
     return result.t, result.y
