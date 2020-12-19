91b12aa33e367ad68ab8cd92896ff1a23ed939a6
Amaar Quadri <amaarquadri@gmail.com>
2020-12-19 11:06:13 -0500

    added random changes for debugging

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import sympy as sp
 from src.physics_system.cart_pole import CartPole
 from src.linear_control.linear_controller import LinearController
 from src.linear_control.laplace_analyzer import LaplaceAnalyzer
-from src.simulation.simulator import simulate
+from src.simulation.simulator import simulate, simulate_direct
 from src.linear_control.utils import to_string
 import matplotlib.pyplot as plt
 from matplotlib import use
@@ -56,9 +56,10 @@ def test_system(controller, x_r_func=None, state_0=None, t_f=10):
 
     laplace_analyzer.analyze_controller(controller.K)
 
-    t_vals, state_vals = simulate(physics_system, controller.C, controller.get_controller_function(x_r_func),
-                                  controller.V, controller.W,
-                                  state_0, t_f)
+    # t_vals, state_vals = simulate(physics_system, controller.C, controller.get_controller_function(x_r_func),
+    #                               controller.V, controller.W,
+    #                               state_0, t_f)
+    t_vals, state_vals = simulate_direct(controller, state_0, t_f)
     forces = [np.dot(controller.K, x_r_func(t_) - state_vals[2 * physics_system.x_dim:, i])[0]
               for i, t_ in enumerate(t_vals)]
     powers = [force * velocity for force, velocity in zip(forces, state_vals[1, :])]
@@ -70,20 +71,22 @@ def test_system(controller, x_r_func=None, state_0=None, t_f=10):
 
 
 def main():
-    cart_pole = CartPole(M=1.994376, m=0.105425, L=0.110996, b=1.6359, g=9.81)
-    controller = LinearController(cart_pole, operating_point=None, C=np.array([[1, 0, 0, 0],
-                                                                               [0, 0, 1, 0]]),
-                                  Q=np.diag([10, 20, 100, 50]), R=np.array([[1]]),
-                                  V=np.diag([0, 0.001, 0, 0.001]), W=0.0001*np.identity(2))
+    phys = CartPole(M=1.994376, m=0.105425, L=0.110996, b=1.6359, g=9.81)
+    controller = LinearController(phys, operating_point=None, C=np.array([[1, 0, 0, 0]]),
+                                  Q=np.diag([1, 1, 10, 100]), R=np.array([[0.0001]]),
+                                  V=0.001*np.identity(4), W=0.001*np.identity(1))
 
-    t_vals, state_vals = test_system(controller, x_r_func=lambda t: [(t // 7) % 2, 0, 0, 0],
+    t_vals, state_vals = test_system(controller, x_r_func=lambda t: [0, 0, 0, 0],
                                      state_0=np.array([0, 0, 0, 0,
-                                                       0, 0, 0, 0]),
-                                     t_f=30)
-    for i, x in enumerate(cart_pole.x):
+                                                       0.1, 0, 0, 0]),
+                                     t_f=20)
+    for i, x in enumerate(phys.x):
         plt.plot(t_vals, state_vals[2 * i, :], label=f'${to_string(x)}$')
-    for i, x in enumerate(cart_pole.x):
-        plt.plot(t_vals, state_vals[2 * (cart_pole.x_dim + i), :], label=f'$\\hat{{{to_string(x)}}}$')
+        # plt.plot(t_vals, state_vals[2 * (phys.x_dim + i), :], label=f'$\\hat{{{to_string(x)}}}$')
+
+    # for i, x in enumerate([sp.diff(x, phys.t) for x in phys.x]):
+    #     plt.plot(t_vals, state_vals[2 * i + 1, :], label=f'${to_string(x)}$')
+        # plt.plot(t_vals, state_vals[2 * (phys.x_dim + i) + 1, :], label=f'$\\hat{{{to_string(x)}}}$')
 
     # plt.plot(t_vals, x_r_func(t_vals)[0], label=r'$x_{r}$')
     plt.xlabel('Time (s)')
--- src/simulation/simulator.py
+++ src/simulation/simulator.py
@@ -19,6 +19,9 @@ def simulate(physics_system, C, controller, V=None, W=None, state_0=None, t_f=10
 
     model_noise = gaussian_noise_function(variances=V)
     control_noise = gaussian_noise_function(variances=W)
+    A, B = physics_system.get_linearized_equations_of_motion()
+    A = physics_system.apply_substitutions(A)
+    B = physics_system.apply_substitutions(B)
 
     def state_derivative(t, state):
         print(t)
@@ -26,12 +29,32 @@ def simulate(physics_system, C, controller, V=None, W=None, state_0=None, t_f=10
         x = state[:midpoint]
         x_hat = state[midpoint:]
 
-        y = np.dot(C, x) + control_noise(t)
+        y = np.dot(C, x)  # + control_noise(t)
         u, x_hat_dot = controller(t, x_hat, y)
-        x_dot = physics_system.equations_of_motion_func(x, u) + model_noise(t)
+        x_dot = np.dot(A, x) + np.dot(B, u)  # physics_system.equations_of_motion_func(x, u)# + model_noise(t)
 
         return np.concatenate((x_dot, x_hat_dot))
 
-    result = solve_ivp(state_derivative, (0, t_f), state_0, method='RK45', rtol=1e99, atol=1e99, max_step=0.001)
+    result = solve_ivp(state_derivative, (0, t_f), state_0, method='RK45', rtol=1e99, atol=1e99, max_step=0.01)
+    # noinspection PyUnresolvedReferences
+    return result.t, result.y
+
+
+def simulate_direct(controller, state_0=None, t_f=10, rtol=1e-3):
+    """
+    :param controller: The controller function which accepts (t, x_hat, y) and returns (u, x_hat_dot).
+    :param state_0: A numpy array of the starting x and x_hat values.
+    :param t_f:
+    :param rtol:
+    """
+    if state_0 is None:
+        state_0 = np.zeros(2 * controller.phys.x_dim)
+
+    M = controller.phys.apply_substitutions(controller.overall_dynamics)
+
+    def state_derivative(_, state):
+        return np.dot(M, state)
+
+    result = solve_ivp(state_derivative, (0, t_f), state_0, method='RK45')  # , rtol=1e99, atol=1e99, max_step=0.01)
     # noinspection PyUnresolvedReferences
     return result.t, result.y
